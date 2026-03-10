ca3b2500d84ebbeee88ca70abbdc73af8d9a01cb
Vitor-Curado <vitorbmcurado@proton.me>
2026-03-09 20:39:47 -0400

    feat: enhance logging and tracing capabilities; update CI/CD pipeline and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use personal_website::router::app;
 use tokio::net::TcpListener;
 use personal_website::config::Config;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let app = app();
     let config = Config::from_env();
     let listener = TcpListener::bind(("0.0.0.0", config.port)).await.unwrap();
--- src/router.rs
+++ src/router.rs
@@ -2,6 +2,7 @@ use crate::state::AppState;
 use axum::Router;
 use axum::routing::get;
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
 
 use crate::handlers::{assets, blog, contact, food, food_detail, health, home, resume};
 
@@ -16,5 +17,6 @@ pub fn app() -> Router {
         .route("/contact", get(contact))
         .route("/assets", get(assets))
         .nest_service("/static", ServeDir::new("static"))
+        .layer(TraceLayer::new_for_http())
         .with_state(AppState::new())
 }
